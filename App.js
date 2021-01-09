d69658943771b2418db1419b55b6c91bdbf4cbf6
angelina-tsuboi <angelina.t1832@gmail.com>
2021-01-09 21:49:14 +0900

    feat: added nav

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as PaperProvider } from 'react-native-paper';
+import Navbar from './Navbar';
 
 export default function App() {
   return (
+    <PaperProvider>
     <View style={styles.container}>
+      <Navbar></Navbar>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </PaperProvider>
   );
 }
 
